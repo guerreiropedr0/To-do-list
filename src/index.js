300f6bfd740c836335b5b6611a82086758446201
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-14 13:09:19 +0000

    Add an array of objects with some simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './style.css';
 
-(() => {
-  document.querySelector('h1').innerHTML = 'Pedro Hello\'s the World! <i class="fas fa-cat"></i>';
-})();
+const TASKS = [
+  {
+    description: 'do the dishes',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'workout',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'clean your room',
+    completed: false,
+    index: 0,
+  },
+];
