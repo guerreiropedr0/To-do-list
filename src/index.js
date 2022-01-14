0ec91599145129874722aa7cc37b8cbc35ca52d2
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-14 14:13:57 +0000

    Fix linters and refactor code

--- src/index.js
+++ src/index.js
@@ -18,16 +18,11 @@ const TASKS = [
   },
 ];
 
-function populateDiv() {
+(() => {
   const LIST = document.getElementById('list');
-  LIST.innerHTML =
-    '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button type="button">Clear all completed</button></form>';
-  TASKS.sort(function (a, b) {
-    return a.index - b.index;
-  });
+  LIST.innerHTML = '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button type="button">Clear all completed</button></form>';
+  TASKS.sort((a, b) => a.index - b.index);
   for (let i = 0; i < TASKS.length; i += 1) {
     LIST.innerHTML += `<div class="task"><input type="checkbox"><textarea>${TASKS[i].description}</textarea></div>`;
   }
-}
-
-populateDiv();
+})();
