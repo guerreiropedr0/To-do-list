c12aaf426e54252731f50123b7cefc4410760e55
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-17 20:09:51 +0000

    Deleting a task updates all remaining item's indexes

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -7,10 +7,12 @@ export default class TaskList {
 
   display() {
     const LIST = document.getElementById('list');
-    LIST.innerHTML = '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button type="button">Clear all completed</button></form>';
+    LIST.innerHTML =
+      '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button type="button">Clear all completed</button></form>';
     this.array.forEach((task) => {
       LIST.innerHTML += `<div class="task"><input type="checkbox"><textarea>${task.description}</textarea><i id="${task.index}" class="far fa-trash-alt"></i></div>`;
     });
+
     const removeBtns = LIST.querySelectorAll('.fa-trash-alt');
     removeBtns.forEach((icon) => {
       icon.addEventListener('click', () => {
@@ -23,7 +25,7 @@ export default class TaskList {
     const task = new Task(
       document.querySelector("input[type='text'").value,
       false,
-      this.array.length + 1,
+      this.array.length + 1
     );
     this.array.push(task);
     this.display();
@@ -31,6 +33,9 @@ export default class TaskList {
 
   removeTask(index) {
     this.array.splice(index, 1);
+    for (let i = index; i < this.array.length; i += 1) {
+      this.array[i].index -= 1;
+    }
     this.display();
   }
 }
