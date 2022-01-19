9e190b56316f304bab8000a310fcb1d3fc8a3887
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-19 21:01:56 +0000

    Add event listener to the checkbox (change)

--- src/modules/taskList.js
+++ src/modules/taskList.js
@@ -8,32 +8,38 @@ export default class TaskList {
 
   display = () => {
     const LIST = document.getElementById('list');
-    LIST.innerHTML = '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button type="button">Clear all completed</button></form>';
+    LIST.innerHTML =
+      '<form><div class="heading"><textarea>Things to do</textarea><input type="text" placeholder="Add to your list..."></div><button class="clear" type="button">Clear all completed</button></form>';
     this.array.forEach((task) => {
-      LIST.innerHTML += `<div class="task"><input type="checkbox"><textarea id="${task.index}">${task.description}</textarea><i id="${task.index}" class="far fa-trash-alt"></i></div>`;
+      LIST.innerHTML += `<div class="task"><input id="${task.index}" type="checkbox"><textarea id="${task.index}">${task.description}</textarea><i id="${task.index}" class="far fa-trash-alt"></i></div>`;
     });
 
-    const removeBtns = LIST.querySelectorAll('.fa-trash-alt');
-    removeBtns.forEach((icon) => {
+    const REMOVE_ICON = LIST.querySelectorAll('.fa-trash-alt');
+    REMOVE_ICON.forEach((icon) => {
       icon.addEventListener('click', () => {
         this.removeTask(icon.id - 1);
       });
     });
-    const editableTask = document.querySelectorAll('.task textarea');
-    editableTask.forEach((task) => {
+    const TEXTAREA = document.querySelectorAll('.task textarea');
+    TEXTAREA.forEach((task) => {
       task.addEventListener('input', () => {
         this.editTask(task.id - 1, task);
       });
     });
+
+    const INPUT_CHECKBOX = document.querySelectorAll('.task input');
+    INPUT_CHECKBOX.forEach((checkbox) => {
+      checkbox.addEventListener('change', () => {});
+    });
   };
 
   addTask = () => {
-    const task = new Task(
+    const TASK = new Task(
       document.querySelector("input[type='text'").value,
       false,
-      this.array.length + 1,
+      this.array.length + 1
     );
-    this.array.push(task);
+    this.array.push(TASK);
     this.display();
     StorageManager.save(this.array);
   };
